5a9eee7257aa8f32823653b5e0471027e093d713
aurieh <me@aurieh.me>
2017-12-15 18:28:56 +0200

    remove outdated safesearch code

--- utils/utils.go
+++ utils/utils.go
@@ -17,29 +17,3 @@ func GetJSON(res *http.Response, target interface{}) error {
 	defer res.Body.Close()
 	return json.NewDecoder(res.Body).Decode(target)
 }
-
-// func SafesearchDisabledChannel(client *commandclient.CommandClient, id string) (bool, error) {
-// 	safesearch := true
-// 	rows, err := client.DB.Query("SELECT * FROM channels WHERE id=?;", id)
-// 	if err != nil {
-// 		return false, err
-// 	}
-// 	for rows.Next() {
-// 		err := rows.Scan(&safesearch)
-// 		if err != nil {
-// 			return false, err
-// 		}
-// 		if !safesearch {
-// 			break
-// 		}
-// 	}
-// 	err = rows.Close()
-// 	if err != nil {
-// 		return false, err
-// 	}
-// 	err = rows.Err()
-// 	if err != nil {
-// 		return false, err
-// 	}
-// 	return safesearch, nil
-// }
